89e3b0b607f64b6837a17d69cf764ed0c9a45380
Suhita <suhitapoonia@gmail.com>
2026-03-29 22:30:45 +0530

    simplify build

--- build.sh
+++ build.sh
@@ -3,9 +3,4 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py shell -c "
-from django.contrib.auth import get_user_model
-User = get_user_model()
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'suhitapoonia@gmail.com', 'pms@2026')
-"
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'suhitapoonia@gmail.com', 'Admin@123')"
